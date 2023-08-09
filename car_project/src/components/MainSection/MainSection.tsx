c6939a5d78d503c08f388370506b9055a12cc4a2
alesha229 <dfyz123123123123@mail.ru>
2023-08-09 13:42:09 +0300

    Menu and first screen

---
+++ car_project/src/components/MainSection/MainSection.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './MainSection.scss'
+
+function MainSection() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='main'>
+        <div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MainSection

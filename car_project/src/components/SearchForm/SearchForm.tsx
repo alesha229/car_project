6aed7428be7a2aaf6cd09f0d0871190d953c0663
alesha229 <dfyz123123123123@mail.ru>
2024-11-22 09:18:39 +0300

    link create

--- car_project/src/components/SearchForm/SearchForm.tsx
+++ car_project/src/components/SearchForm/SearchForm.tsx
@@ -1,65 +1,85 @@
-import { FC, useEffect, useState } from 'react'
-import CustomSelect from '../CustomSelect/CustomSelect'
-import './SearchForm.scss'
-import { AppDispatch, useAppSelector } from '../../store/store'
-import { fetchSelect } from '../../store/slices/сarSlice'
-import { useDispatch } from 'react-redux'
-import { resolvePath } from '../../utils/resolvePath'
+import { FC, useEffect, useState } from "react";
+import CustomSelect from "../CustomSelect/CustomSelect";
+import "./SearchForm.scss";
+import { AppDispatch, useAppSelector } from "../../store/store";
+import { fetchSelect, fetchSelectCars } from "../../store/slices/сarSlice";
+import { useDispatch } from "react-redux";
+import { resolvePath } from "../../utils/resolvePath";
+import MainButton from "../MainButton/MainButton";
+import SearchResult from "../SearchResult/SearchResult";
 
 interface IOption {
-	value: string
-	label: string
+  value: string;
+  label: string;
 }
 
 const SearchForm: FC = () => {
-	const [selectedBrand, setSelectedBrand] = useState<string>('')
-	const [selectedModel, setSelectedModel] = useState<string>('')
-	const [modelOptions, setModelOptions] = useState<IOption[]>([
-		{
-			"value": "none",
-			"label": "none"
-		}
-	])
-	const selects = useAppSelector(state => state.cars.selects)
-	const dispatch = useDispatch<AppDispatch>()
+  const [selectedBrand, setSelectedBrand] = useState<string>("");
+  const [selectedModel, setSelectedModel] = useState<string>("");
+  const [selectedCars, setSelectedCars] = useState<string>("");
+  const [modelOptions, setModelOptions] = useState<IOption[]>([
+    {
+      value: "none",
+      label: "none",
+    },
+  ]);
+  const selects = useAppSelector((state) => state.cars.selects);
+  const results = useAppSelector((state) => state.cars.selects);
+  const dispatch = useDispatch<AppDispatch>();
+  const states = {
+    brand: selectedBrand,
+    model: selectedModel,
+    // options: modelOptions
+  };
+  useEffect(() => {
+    dispatch(fetchSelect());
+  }, []);
+  useEffect(() => {
+    if (selectedBrand != "") {
+      setModelOptions(
+        Object.keys(resolvePath(selectedBrand, selects)).map(
+          (brand: string) => ({
+            value: brand,
+            label: brand,
+          })
+        )
+      );
+    }
+    setSelectedModel("");
+  }, [selectedBrand]);
+  const brandOptions: IOption[] = Object.keys(selects).map((brand: string) => ({
+    value: brand,
+    label: brand,
+  }));
 
-	useEffect(() => {
-		dispatch(fetchSelect())
-	}, [])
-	useEffect(() => {
-		if (selectedBrand != '') {
-			setModelOptions(
-				Object.keys(resolvePath(selectedBrand, selects)).map((brand: string) => ({
-					value: brand,
-					label: brand,
-				}),))
-		}
-		setSelectedModel("")
-	}, [selectedBrand])
-	const brandOptions: IOption[] = Object.keys(selects).map((brand: string) => ({
-		value: brand,
-		label: brand,
-	}),)
+  function findCar() {
+    console.log(states);
+    dispatch(fetchSelectCars(states));
+  }
+  return (
+    <div className="form">
+      <CustomSelect
+        placeholder="Марка авто"
+        options={brandOptions}
+        selectedOption={selectedBrand}
+        onOptionChange={setSelectedBrand}
+      />
+      <CustomSelect
+        placeholder="Модель"
+        options={modelOptions}
+        selectedOption={selectedModel}
+        onOptionChange={setSelectedModel}
+        isDisabled={selectedBrand == "" ? true : false}
+      />
+      <MainButton
+        onClick={() => findCar()}
+        label="Поиск"
+        arrow={false}
+        btnStyle="SmallButton"
+        link="any"
+      />
+    </div>
+  );
+};
 
-
-	return (
-		<div className='form'>
-			<CustomSelect
-				placeholder='Марка авто'
-				options={brandOptions}
-				selectedOption={selectedBrand}
-				onOptionChange={setSelectedBrand}
-			/>
-			<CustomSelect
-				placeholder='Модель'
-				options={modelOptions}
-				selectedOption={selectedModel}
-				onOptionChange={setSelectedModel}
-				isDisabled={(selectedBrand == '') ? true : false}
-			/>
-
-		</div>
-	)
-}
-
-export default SearchForm
+export default SearchForm;
--- car_project/src/store/slices/сarSlice.ts
+++ car_project/src/store/slices/сarSlice.ts
@@ -1,65 +1,100 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { ICar, ISelectOption } from '../../types'
-import axios from '../../utils/axios'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { ICar, ISelectOption } from "../../types";
+import axios from "../../utils/axios";
 
 interface ICarCardState {
-	cars: ICar[]
-	selects: ISelectOption[]
-	status: string
+  cars: ICar[];
+  selects: ISelectOption[];
+  status: string;
+  results: ICar[];
 }
 
-export const fetchAllCars = createAsyncThunk('cars/fetchAllCars', async () => {
-	const { data } = await axios.get('/cars')
-	return data
-})
+export const fetchAllCars = createAsyncThunk("cars/fetchAllCars", async () => {
+  const { data } = await axios.get("/cars");
+  return data;
+});
 
-export const fetchSelect = createAsyncThunk('select/fetchSelect', async () => {
-	const { data } = await axios.get('/select')
-	return data
-})
+export const fetchSelect = createAsyncThunk("select/fetchSelect", async () => {
+  const { data } = await axios.get("/select");
+  return data;
+});
 
+export const fetchSelectCars = createAsyncThunk(
+  "select/fetchSelectCars",
+  async (states: any) => {
+    let baseLink = `/cars?`;
+    let isfirst: boolean = true;
+    for (const state in states) {
+      if (state != "") {
+        if (isfirst) {
+          baseLink += state + `=` + states[state];
+          isfirst = false;
+        } else {
+          baseLink += `&` + state + `=` + states[state];
+        }
+      }
+    }
+    const { data } = await axios.get(baseLink);
+    console.log(data);
+    return data;
+  }
+);
 const initialState: ICarCardState = {
-	cars: [],
-	selects: [],
-	status: 'loading',
-}
+  cars: [],
+  selects: [],
+  status: "loading",
+  results: [],
+};
 
 export const carsSlice = createSlice({
-	name: 'cars',
-	initialState,
-	reducers: {},
-	extraReducers: builder => {
-		builder
-			// fetchAllCars
-			.addCase(fetchAllCars.pending, (state, action) => {
-				state.cars = []
-				state.status = 'loading'
-			})
-			.addCase(fetchAllCars.fulfilled, (state, action) => {
-				state.cars = action.payload
-				state.status = 'success'
-			})
-			.addCase(fetchAllCars.rejected, (state, action) => {
-				state.cars = []
-				state.status = 'error'
-			})
+  name: "cars",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // fetchAllCars
+      .addCase(fetchAllCars.pending, (state, action) => {
+        state.cars = [];
+        state.status = "loading";
+      })
+      .addCase(fetchAllCars.fulfilled, (state, action) => {
+        state.cars = action.payload;
+        state.status = "success";
+      })
+      .addCase(fetchAllCars.rejected, (state, action) => {
+        state.cars = [];
+        state.status = "error";
+      })
 
-			// fetchSelect
-			.addCase(fetchSelect.pending, (state, action) => {
-				state.selects = []
-				state.status = 'loading'
-			})
-			.addCase(fetchSelect.fulfilled, (state, action) => {
-				state.selects = action.payload
-				state.status = 'success'
-			})
-			.addCase(fetchSelect.rejected, (state, action) => {
-				state.selects = []
-				state.status = 'error'
-			})
-	},
-})
+      // fetchSelect
+      .addCase(fetchSelect.pending, (state, action) => {
+        state.selects = [];
+        state.status = "loading";
+      })
+      .addCase(fetchSelect.fulfilled, (state, action) => {
+        state.selects = action.payload;
+        state.status = "success";
+      })
+      .addCase(fetchSelect.rejected, (state, action) => {
+        state.selects = [];
+        state.status = "error";
+      })
+      // fetchSelectCars
+      .addCase(fetchSelectCars.pending, (state, action) => {
+        state.selects = [];
+        state.status = "loading";
+      })
+      .addCase(fetchSelectCars.fulfilled, (state, action) => {
+        state.selects = action.payload;
+        state.status = "success";
+      })
+      .addCase(fetchSelectCars.rejected, (state, action) => {
+        state.selects = [];
+        state.status = "error";
+      });
+  },
+});
 
-export const {} = carsSlice.actions
+export const {} = carsSlice.actions;
 
-export default carsSlice.reducer
+export default carsSlice.reducer;
